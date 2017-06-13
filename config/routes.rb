45a6b5ff04e83bb3ae77ed9c3e189c9161051e0f
Diego Carretero Alvarez <carretero.alv.diego@gmail.com>
2017-06-13 16:56:24 -0500

    Add custom routes for portfolio resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'contact' => 'pages#contact'
 
   resources :blogs
-  resources :portfolios
+  resources :portfolios, except: [:show, :edit]
+  get 'portfolio/:id' => 'portfolios#show', as: "portfolio_show"
+  get 'portfolio/:id/edit' => 'portfolios#edit', as: "portfolio_edit"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
