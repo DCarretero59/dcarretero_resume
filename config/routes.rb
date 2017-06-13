3b07982623145cb2f13ea01262133fdb004e3162
Diego Carretero Alvarez <carretero.alv.diego@gmail.com>
2017-06-13 17:33:59 -0500

    Integrated friendly routes for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
 
-  resources :blogs
+  resources :blogs, except: [:show, :edit]
+  get 'blog/:id' => 'blogs#show', as: "blog_show"
+  get 'blog/:id/edit' => 'blogs#edit', as: "blog_edit"
   resources :portfolios, except: [:show, :edit]
   get 'portfolio/:id' => 'portfolios#show', as: "portfolio_show"
   get 'portfolio/:id/edit' => 'portfolios#edit', as: "portfolio_edit"
