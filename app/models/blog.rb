a670af8d3aeed81ab09d9bc439cadd3ac30d133b
Diego Carretero Alvarez <carretero.alv.diego@gmail.com>
2017-06-28 18:30:49 -0500

    Integrated helper methods from ActionView

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,11 +1,17 @@
 class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
+  after_initialize :set_defaults
 
   enum status: {
     draft:     0,
     published: 1
   }
-  validates_presence_of :title, :body
-  belongs_to :topic
+  validates_presence_of :title, :body, :topic_id
+  belongs_to :topic#, optional: true
+  
+  private 
+    def set_defaults
+      self.topic_id ||= Topic.order("Rand()").first.id
+    end
 end
